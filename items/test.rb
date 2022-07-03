c7d4715b33ff99845f73843428b3073716e3ab50
AndriiStegnytskyi <a.stegnytskyi@gmail.com>
2022-07-03 14:03:41 +0200

    added logic ini test.rb and spec_helper

--- items/test.rb
+++ items/test.rb
@@ -2,3 +2,13 @@
 # Створіть клас Test і в ньому метод run
 # В метод run буде приходити параметр з іменем тесту
 # return має бути результат тесту
+
+class Test
+def run (test_name)
+  result = "bin/rspec spec/#{test_name}"
+  puts result
+end
+
+end
+test = Test.new
+test.run("KostiaFedorenko/Fedorenko_kata1_spec.rb")
