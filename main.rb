37353388abb9f75714dd344958409229d930a707
Владислав Малко <93429979+PrincTwilig@users.noreply.github.com>
2022-07-03 22:19:32 +0300

    GetStarted main.rb

--- main.rb
+++ main.rb
@@ -1,1 +1,78 @@
 # Виклик кат і робота з консоллю
+
+# розкоментувати рядок нижче щоб імпортувати всі файли з папки items
+# Dir["./items/*.rb"].each {|file| require file }
+
+
+class Main
+    def initialize
+        @choose = ""
+        @argument = ""
+
+        # Ініціалізація всіх функцій   (Розкоментовувати як тільки появляються класи)
+        # @help = Help.new
+        # @run = Run.new
+        # @show = Show.new
+        # @specs = Specs.new
+        # @tasks = Tasks.new
+        # @test = Test.new
+        # @tests = Tests.new
+
+        Logo()
+        # Starts main loop
+        Loop()
+    end
+
+    def Loop
+        while true
+            print "> "
+            buff = gets.chomp.downcase
+            @choose = buff.split(" ")[0]
+            @argument = buff.split(" ")[1]
+            
+            case @choose
+            when "help"
+                # @help.run(@argument)
+            when "run"
+                # @run.run(@argument)
+            when "show"
+                # @show.run(@argument)
+            when "specs"
+                # @specs.run(@argument)
+            when "tasks"
+                # @tasks.run(@argument)
+            when "test"
+                # @test.run(@argument)
+            when "tests"
+                # @tests.run(@argument)
+            when "logo"
+                Logo()
+            when "clear"
+                system("cls")
+            when "exit"
+                puts "\nBye!\n"
+                break
+            else
+                puts "Wrong command! Type 'help' to see list of commands."
+            end
+        end
+    end
+
+    def Logo
+        system("cls")
+        puts "                              @@@@                                                                        "
+        puts "                             @@      @                                                                    "
+        puts "                            :@       @          ,           ,,                                    ,       "
+        puts "  @@h5#@@     M@@BX#@@9    @@@@@@; @@@@@@;  @#@@@@@@s   :@@@@@@@#    A@@ M@@@ @@@S     @@@    @@@@@@@@    "
+        puts " @A          @#       #@    ,@       @     @@@     :   &@@#    @@#   A#@9@@s;  @@@    9@@B  5@@M    @@@   "
+        puts " M@,        @B         @A   :@       @     3#@#        @@#     .@@&  A@@#      A#@5   @@#   @@@      @@@  "
+        puts "   :@@@.    @          @@   :@       @       B@@@@#   X@@@@@@@@@@@B  A@@5       @@@  #@@,   @@@@#@@@@@@@  "
+        puts "       @@   @H         @H   :@       @           @@@B  @@@           A@@:        @@@ #@#    @@@           "
+        puts "        #.   @M       @#    :@       @      @.   H@@#  r@@r    @G   A@@:        H@@#@#     .#@#B    @@    "
+        puts " #@#;,B@@     @@@Xrh@M     :@       i@@@@ @#@@@@@@H     @@@@@@@@@   A@@:         @@@@&       @@#@@@@@@    "
+        puts "\n\n\n\n"
+        puts "Type 'help' to see list of commands:"
+    end
+end
+
+Main.new
