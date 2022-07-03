d57d0daf31f6fb49b65f0892a005ef41053d6e54
Vladyslav Denysiuk <vl.denysiuk@gmail.com>
2022-07-03 17:22:27 +0300

    Add Denysiuk katas and tests, add tests.rb

--- items/tests.rb
+++ items/tests.rb
@@ -1,3 +1,12 @@
 # Тут потрібно створити програму яка буде повертати результати всіх тестів
 # Створіть клас Test і в ньому метод run
-# return має бути результат всіх тестів в вигляді строки 
+# return має бути результат всіх тестів в вигляді строки
+Class Tests
+  def run  
+    tests_array =  Dir['**/*_{spec.rb}']
+    tests_array.map do |file| 
+      file = File.basename(file)
+    end
+    tests_array
+  end
+end
