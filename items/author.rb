98134693a92879d2584f3beb5c8a308787c23779
Vladyslav Denysiuk <vl.denysiuk@gmail.com>
2022-07-05 00:33:29 +0300

    Refactor code in author and fixed bug in tests

--- items/author.rb
+++ items/author.rb
@@ -1,60 +1,60 @@
 # Show the author of a chosen kata/test
 # Works as follows:
-# entry example: "author 'Hryniv_kata1.rb'"
+# entry example: "author 'Hryniv_kata1'" (no file extension needed)
 # returns name, surname and github of the author
 
 class Author
   def run(file)
-    surname = file.split("_")[0]
+    surname = file.split("_")[0].downcase
     case surname
-    when "Hryniv"
-      if File.file?("katas/AnastasiiaHryniv/#{file}") ||
-         File.file?("spec/AnastasiiaHryniv/#{file}")
+    when "hryniv"
+      if File.file?("katas/AnastasiiaHryniv/#{file}.rb") ||
+         File.file?("spec/AnastasiiaHryniv/#{file}.rb")
         author = "Author: Anastasiia Hryniv\nGithub: anastasiiahryniv"
       end
-    when "Stegnytskyi"
-      if File.file?("katas/AndriiStegnytskyi/#{file}") ||
-         File.file?("spec/AndriiStegnytskyi/#{file}")
+    when "stegnytskyi"
+      if File.file?("katas/AndriiStegnytskyi/#{file}.rb") ||
+         File.file?("spec/AndriiStegnytskyi/#{file}.rb")
         author = "Author: Andrii Stegnytskyi\nGithub: AndriiStegnytskyi"
       end
-    when "Koval"
-      if File.file?("katas/IrynaKoval/#{file}") ||
-         File.file?("spec/IrynaKoval/#{file}")
+    when "koval"
+      if File.file?("katas/IrynaKoval/#{file}.rb") ||
+         File.file?("spec/IrynaKoval/#{file}.rb")
         author = "Author: Irina Koval\nGithub: Beerka1"
       end
-    when "Fedorenko"
-      if File.file?("katas/KostiaFedorenko/#{file}") ||
-         File.file?("spec/KostiaFedorenko/#{file}")
+    when "fedorenko"
+      if File.file?("katas/KostiaFedorenko/#{file}.rb") ||
+         File.file?("spec/KostiaFedorenko/#{file}.rb")
         author = "Author: Kostia Fedorenko\nGithub: KostiaFed"
       end
-    when "Sivachenko"
-      if File.file?("katas/OlexiySivachenko/#{file}") ||
-         File.file?("spec/OlexiySivachenko/#{file}")
+    when "sivachenko"
+      if File.file?("katas/OlexiySivachenko/#{file}.rb") ||
+         File.file?("spec/OlexiySivachenko/#{file}.rb")
         author = "Author: Olexiy Sivachenko\nGithub: malk1ss"
       end
-    when "Korkonishko"
-      if File.file?("katas/RomanKorkonishko/#{file}") ||
-         File.file?("spec/RomanKorkonishko/#{file}")
+    when "korkonishko"
+      if File.file?("katas/RomanKorkonishko/#{file}.rb") ||
+         File.file?("spec/RomanKorkonishko/#{file}.rb")
         author = "Author: Roman Korkonishko\nGithub: kromch"
       end
-    when "Malko"
-      if File.file?("katas/VladMalko/#{file}") ||
-         File.file?("spec/VladMalko/#{file}")
+    when "malko"
+      if File.file?("katas/VladMalko/#{file}.rb") ||
+         File.file?("spec/VladMalko/#{file}.rb")
         author = "Author: Vlad Malko\nGithub: PrincTwilig"
       end
-    when "Denysiuk"
-      if File.file?("katas/VladyslavDenysiuk/#{file}") ||
-         File.file?("spec/VladyslavDenysiuk/#{file}")
+    when "denysiuk"
+      if File.file?("katas/VladyslavDenysiuk/#{file}.rb") ||
+         File.file?("spec/VladyslavDenysiuk/#{file}.rb")
         author = "Author: Vladyslav Denysiuk\nGithub: denysiuk"
       end
-    when "Shevtsov"
-      if File.file?("katas/DmitriyShevtsov/#{file}") ||
-         File.file?("spec/DmitriyShevtsov/#{file}")
+    when "shevtsov"
+      if File.file?("katas/DmitriyShevtsov/#{file}.rb") ||
+         File.file?("spec/DmitriyShevtsov/#{file}.rb")
         author = "Author: Dmitriy Shevtsov\nGithub: DmitriyShev"
       end
-    when "Farat"
-      if File.file?("katas/OleksiiFarat/#{file}") ||
-         File.file?("spec/OleksiiFarat/#{file}")
+    when "farat"
+      if File.file?("katas/OleksiiFarat/#{file}.rb") ||
+         File.file?("spec/OleksiiFarat/#{file}.rb")
         author = "Author: Oleksii Farat\nGithub: Oleksii-Farat"
       end
     end
--- items/tests.rb
+++ items/tests.rb
@@ -1,7 +1,7 @@
 # Тут потрібно створити програму яка буде повертати результати всіх тестів
 # Створіть клас Test і в ньому метод run
 # return має бути результат всіх тестів в вигляді строки
-Class Tests
+class Tests
   def run  
     tests_array =  Dir['**/*_{spec.rb}']
     tests_array.map do |file| 
